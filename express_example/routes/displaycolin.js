bd4d387f2c77c23ab9f55fa9154d271808b1dba2
Lucas Meier <meiluca@dunwoody.edu>
2021-03-17 13:46:35 -0500

    changes

--- express_example/routes/displaycolin.js
+++ express_example/routes/displaycolin.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var data = require('../datastore/activity.json');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('displaycolin', { title: 'Hi Colin' });
+
+   
+    res.render('displaycolin', data);
 });
 
 module.exports = router;
